8263531abf93f308bf243bfb1846872f3c492586
Olya <olapancisina1@gmail.com>
2024-06-06 11:45:59 +0200

    add portfolio block + adaptive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import AboutMe from "@/components/about-me";
 import Educations from "@/components/educations";
 import FirstScreen from "@/components/first-screen";
-import Skills from '@/components/skills'
+import Portfolio from "@/components/portfolio";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <section>
       <FirstScreen />
       <AboutMe />
-      <Educations />
       <Skills />
+      <Educations />
+      <Portfolio />
     </section>
   );
 }
--- src/components/educations.tsx
+++ src/components/educations.tsx
@@ -5,7 +5,7 @@ export default function Educations() {
   return (
     <section className="container pb-[120px]">
       <div className="flex flex-col items-center">
-        <TextWithCircle text="Education" className="ml-0 before:left-[28%]" />
+        <TextWithCircle text="Education" className="ml-0 before:left-[25%] md:before:left-[28%]" />
         <Title text="My courses" className="m-auto mt-[10px]" />
         <div className="mt-[20px] text-[#ffffff7d]">2022 - 2024</div>
       </div>
